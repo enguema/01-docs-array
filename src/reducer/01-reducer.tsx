3884743964017943862c7f090a40885a6b66a0c3
enguema <emmanguema@hotmail.com>
2026-06-15 19:43:44 +0100

    reducer 1

--- src/reducer/01-reducer.tsx
+++ src/reducer/01-reducer.tsx
@@ -1,27 +1,52 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useReducer } from "react";
 
-function reducer(state:any, action:any) {
-    if (action.type === 'increment') {
-        return { age: state.age + 1 }
+function reducer(state: any, action: any) {
+    switch (action.type) {
+        case 'increment_age': {
+            //calcula y devuelve el siguiente estado
+            return { name: state.name, age: state.age + 1 }
+        }
+
+        case 'changed_name': {
+            return { name: action.nextName, age: state.age }
+        }
     }
+
     throw Error('Accion desconocida');
+    /*if (action.type === 'increment') {
+        return { age: state.age + 1 }
+    }*/
 }
 
 export default function MyreducerComponent() {
     const [state, dispatch] = useReducer(reducer, { age: 42 });
+
+    function handleButtonClick() {
+        dispatch({ type: "increment_age" });
+    }
+
+    function handleInputChange(e) {
+        dispatch(
+            { type: 'changed_name', nextName: e.target.value }
+        )
+    }
 
     return (
         <>
             <div>
                 <h2>Using Reducer</h2>
 
-                <Button onClick={() => {
-                    dispatch({ type: "increment" })
-                }}>
+                <Input
+                    onChange={handleInputChange}
+                    value={state.name}
+                />
+                <Button onClick={handleButtonClick}>
                     Increment age
                 </Button>
-                <p>Hello! You are: {state.age}</p>
+
+                <p>Hello {state.name}! You are: {state.age}</p>
             </div>
         </>
     );
