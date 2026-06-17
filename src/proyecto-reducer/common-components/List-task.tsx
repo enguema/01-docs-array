a82d635dc362931caef4fd1919e5d58e621357e1
enguema <emmanguema@hotmail.com>
2026-06-17 22:59:49 +0100

    ok

--- src/proyecto-reducer/common-components/List-task.tsx
+++ src/proyecto-reducer/common-components/List-task.tsx
@@ -2,13 +2,13 @@ import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
 export default function ListTask() {
-    const [artist, setArtist] = useState([]);
+    const [artist, setArtist] = useState(initialTasks);
     return (
         <>
             <div className="div-md">
                 <ul className="l-padding">
                     {artist.map(art => (
-                        <li key={art.id}>{art.name}
+                        <li key={art.id}>{art.text}
                             <Button className="bg-red-500 " onClick={()=>{
                                 setArtist(
                                     artist.filter(a=> 
@@ -26,3 +26,9 @@ export default function ListTask() {
         </>
     );
 }
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: 'Visitar el Museo Kafka', done: true },
+  { id: 1, text: 'Ver espectáculo de títeres', done: false },
+  { id: 2, text: 'Foto del muro de Lennon', done: false }
+];
